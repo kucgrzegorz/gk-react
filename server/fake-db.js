ea8aeb8cc20833bbd67db7d094b8b3e628c0986d
unknown <grzechoslaw@gmail.com>
2019-03-04 20:07:07 +0100

    Adds endpoint for creation of arcade and search arcade by city|

--- server/fake-db.js
+++ server/fake-db.js
@@ -1,58 +1,20 @@
 const Arcade = require('./models/arcade');
 const User = require('./models/user');
+const Booking = require('./models/booking');
+
+const fakeDbData = require('./data.json');
 
 class FakeDb {
 
 	constructor(){
-		this.arcades = [{
-                  title: "Nice view on ocean",
-                  city: "San Francisco",
-                  street: "Main street",
-                  category: "condo",
-                  image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-                  stations: 4,
-                  shared: true,
-                  description: "Very nice apartment in center of the city.",
-                  dailyRate: 43
-                  },
-                  {
-                  title: "Modern apartment in center",
-                  city: "New York",
-                  street: "Time Square",
-                  category: "apartment",
-                  image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-                  stations: 1,
-                  shared: false,
-                  description: "Very nice apartment in center of the city.",
-                  dailyRate: 11
-                  },
-                  {
-                  title: "Old house in nature",
-                  city: "Spisska Nova Ves",
-                  street: "Banicka 1",
-                  category: "house",
-                  image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-                  stations: 5,
-                  shared: true,
-                  description: "Very nice apartment in center of the city.",
-                  dailyRate: 23
-            }];
-                  
-            this.users = [{
-                  username: "Test User",
-                  email: "test@gmail.com",
-                  password: "testtest"
-            }, {
-                  username: "Test User1",
-                  email: "test1@gmail.com",
-                  password: "testtest1"
-            }];
-
+		this.arcades = fakeDbData.arcades;
+            this.users = fakeDbData.users;           
 	}
 
 	async cleanDb() {
             await User.deleteMany({});
 		await Arcade.deleteMany({});
+            await Booking.deleteMany({});
 	}
 
 	pushDataToDb() {
